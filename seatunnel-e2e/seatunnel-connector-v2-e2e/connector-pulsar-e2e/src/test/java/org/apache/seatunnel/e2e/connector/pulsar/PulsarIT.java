a04b2699157ebcaa1f819fa80035ae68fa073b83
laglangyue <laglanyue@foxmail.com>
2023-02-17 20:57:05 +0800

    [e2e] pulsar e2e

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-pulsar-e2e/src/test/java/org/apache/seatunnel/e2e/connector/pulsar/PulsarIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-pulsar-e2e/src/test/java/org/apache/seatunnel/e2e/connector/pulsar/PulsarIT.java
@@ -5,6 +5,13 @@ import org.apache.seatunnel.e2e.common.TestSuiteBase;
 
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.pulsar.client.api.Consumer;
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.Schema;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PulsarContainer;
@@ -23,7 +30,12 @@ public class PulsarIT extends TestSuiteBase implements TestResource {
     private static final Integer PULSAR_BROKER_PORT = 6650;
     private static final Integer PULSAR_BROKER_HTTP_PORT = 8080;
     public static final String PULSAR_HOST = "pulsar_e2e";
+    public static final String TOPIC = "topic-it";
+    public static final String CONSUMER_SUBSCRIPTION = "e2e_consumer";
     private PulsarContainer pulsarContainer;
+    private PulsarClient client;
+    private Producer<String> producer;
+    private Consumer<String> consumer;
 
 
     @Override
@@ -37,6 +49,7 @@ public class PulsarIT extends TestSuiteBase implements TestResource {
             String.format("%s:%s", PULSAR_BROKER_PORT, PULSAR_BROKER_PORT),
             String.format("%s:%s", PULSAR_BROKER_HTTP_PORT, PULSAR_BROKER_HTTP_PORT)
         ));
+
         Startables.deepStart(Stream.of(pulsarContainer)).join();
         Awaitility.given()
             .ignoreExceptions()
@@ -49,15 +62,33 @@ public class PulsarIT extends TestSuiteBase implements TestResource {
     @Override
     public void tearDown() throws Exception {
         pulsarContainer.close();
+        client.close();
+        producer.close();
+        consumer.close();
     }
 
     @Test
-    void testPull() {
-        System.out.println(pulsarContainer.getHttpServiceUrl());
-        System.out.println(pulsarContainer.getPulsarBrokerUrl());
+    void testImage() throws PulsarClientException {
+        String hello = "hello world";
+        producer.send(hello);
+        Message<String> receive = consumer.receive();
+        String data = receive.getValue();
+        Assertions.assertEquals(hello, data);
     }
 
-    private void initTopic() {
+    private void initTopic() throws PulsarClientException {
 
+        client = PulsarClient.builder()
+            .serviceUrl(pulsarContainer.getPulsarBrokerUrl())
+            .build();
+
+        producer = client.newProducer(Schema.STRING)
+            .topic(TOPIC)
+            .create();
+
+        consumer = client.newConsumer(Schema.STRING)
+            .topic(TOPIC)
+            .subscriptionName(CONSUMER_SUBSCRIPTION)
+            .subscribe();
     }
 }
